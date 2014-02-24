04674352aadae8f873172aa1456fb201fc0e2e73
Michelle Wegenstein <mwegenstein@gmail.com>
2014-02-24 15:12:16 -0800

    added angular functionality for add button
    when you click the add button the item is added to the list

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,6 +5,12 @@ angular.module('mytodoApp')
     $scope.todos = [
       'Item 1',
       'Item 2',
-      'OHEMGEE Item 3!'
+      'Item 3!'
     ];
+    $scope.addTodo = function () { //addTodo function pushes new todo items onto the existing todo items array and then clears the text input field
+    	$scope.todos.push($scope.todo);
+    	$scope.todo ='';
+    };
   });
+  // TODO Michelle: if you enter more than one blank todo item or a todo item with the same name the list stops working. enhance addTodo with error checking
+  

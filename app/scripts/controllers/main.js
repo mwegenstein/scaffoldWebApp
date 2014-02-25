b0cb9263e4349834a597403a415af7662de6de0b
Michelle Wegenstein <mwegenstein@gmail.com>
2014-02-25 10:54:53 -0800

    removing todo items
    using the splice method created a function to remove one todo item for
    the item array

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,6 +11,9 @@ angular.module('mytodoApp')
     	$scope.todos.push($scope.todo);
     	$scope.todo ='';
     };
+    $scope.removeTodo = function (index) {
+    	$scope.todos.splice(index, 1); //removes one todo item from the items array using the JavaScript splice method at the given $index value
+    };
   });
   // TODO Michelle: if you enter more than one blank todo item or a todo item with the same name the list stops working. enhance addTodo with error checking
-  
+

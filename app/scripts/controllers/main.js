ba6e2f449e9c50280e35c8997c454482eae87d64
Michelle Wegenstein <mwegenstein@gmail.com>
2014-03-03 13:25:24 -0800

    added local storage to refresh page and leave todo list there
    -fixed bug - removed angular-ui/angular-ui.js - unnecessary file was
    breaking page on each refresh. only need angular-ui.js
    -added local storage to maintain list

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,19 +1,25 @@
 'use strict';
 
 angular.module('mytodoApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.todos = [
-      'Item 1',
-      'Item 2',
-      'Item 3!'
-    ];
+  .controller('MainCtrl', function ($scope, localStorageService) { //rather than reading our todos from a static array, it reads from Local Storage and then storing it in $scope.todos instead.
+    
+    var todosInStore = localStorageService.get('todos');
+
+    $scope.todos = todosInStore && todosInStore.split('\n') || [];
+
+    $scope.$watch('todos', function () {
+    	localStorageService.add('todos', $scope.todos.join('\n'));
+    }, true);
+
     $scope.addTodo = function () { //addTodo function pushes new todo items onto the existing todo items array and then clears the text input field
     	$scope.todos.push($scope.todo);
     	$scope.todo ='';
     };
+
     $scope.removeTodo = function (index) {
     	$scope.todos.splice(index, 1); //removes one todo item from the items array using the JavaScript splice method at the given $index value
     };
+    
   });
   // TODO Michelle: if you enter more than one blank todo item or a todo item with the same name the list stops working. enhance addTodo with error checking
 

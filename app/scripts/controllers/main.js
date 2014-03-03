5db24b0805cc3e93ac1c7c95ac5c05acd2923625
Michelle Wegenstein <mwegenstein@gmail.com>
2014-03-03 14:28:23 -0800

    adding Karma to write unit tests

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,25 +1,18 @@
 'use strict';
 
 angular.module('mytodoApp')
-  .controller('MainCtrl', function ($scope, localStorageService) { //rather than reading our todos from a static array, it reads from Local Storage and then storing it in $scope.todos instead.
-    
-    var todosInStore = localStorageService.get('todos');
+  .controller('MainCtrl', function ($scope) {
 
-    $scope.todos = todosInStore && todosInStore.split('\n') || [];
+  $scope.todos = [];
 
-    $scope.$watch('todos', function () {
-    	localStorageService.add('todos', $scope.todos.join('\n'));
-    }, true);
+  $scope.addTodo = function () {
+    $scope.todos.push($scope.todo);
+    $scope.todo = '';
+  };
 
-    $scope.addTodo = function () { //addTodo function pushes new todo items onto the existing todo items array and then clears the text input field
-    	$scope.todos.push($scope.todo);
-    	$scope.todo ='';
-    };
-
-    $scope.removeTodo = function (index) {
-    	$scope.todos.splice(index, 1); //removes one todo item from the items array using the JavaScript splice method at the given $index value
-    };
-    
-  });
+  $scope.removeTodo = function (index) {
+    $scope.todos.splice(index, 1);
+  };
+});
   // TODO Michelle: if you enter more than one blank todo item or a todo item with the same name the list stops working. enhance addTodo with error checking
 
